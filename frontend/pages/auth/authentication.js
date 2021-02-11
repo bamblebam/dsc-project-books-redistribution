b4c20a97b655f6984729fd242031f2455d451749
bamblebam <burhanizrangwala@gmail.com>
2021-02-12 01:15:32 +0530

    first form

--- frontend/pages/auth/authentication.js
+++ frontend/pages/auth/authentication.js
@@ -2,24 +2,44 @@ import React from 'react'
 import Head from 'next/head'
 import styles from '../../styles/css/authentication.module.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faUser } from "@fortawesome/free-solid-svg-icons"
+import { faUser, faLock } from "@fortawesome/free-solid-svg-icons"
+import { faFacebook, faGoogle, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons'
 
 export default function authentication() {
     return (
         <>
             <Head>
-                <script src="https://use.fontawesome.com/ffe70d5ceb.js"></script>
             </Head>
             <div className={styles.body}>
                 <div className={styles.main_container}>
                     <div className={styles.form_container}>
                         <div className={styles.signin_signup}>
                             <form action="" method="POST" className={styles.signin_form}>
-                                <h2 className={styles.title}>
-                                    <div className={styles.input_field}>
-                                        <FontAwesomeIcon icon={faUser}></FontAwesomeIcon>
-                                    </div>
-                                </h2>
+                                <h2 className={styles.title}>Sign In</h2>
+                                <div className={styles.input_field}>
+                                    <FontAwesomeIcon icon={faUser}></FontAwesomeIcon>
+                                    <input type="text" name="signin_username" className={styles.input} placeholder="Username" />
+                                </div>
+                                <div className={styles.input_field}>
+                                    <FontAwesomeIcon icon={faLock}></FontAwesomeIcon>
+                                    <input type="text" name="signin_password" className={styles.input} placeholder="Password" />
+                                </div>
+                                <button type="submit" className={styles.btn, styles.solid}>Sign In</button>
+                                <p className={styles.social_text}>Or sign in with</p>
+                                <div className={styles.social_media}>
+                                    <a href="" className={styles.social_icon}>
+                                        <FontAwesomeIcon icon={faFacebook}></FontAwesomeIcon>
+                                    </a>
+                                    <a href="" className={styles.social_icon}>
+                                        <FontAwesomeIcon icon={faGoogle}></FontAwesomeIcon>
+                                    </a>
+                                    <a href="" className={styles.social_icon}>
+                                        <FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon>
+                                    </a>
+                                    <a href="" className={styles.social_icon}>
+                                        <FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon>
+                                    </a>
+                                </div>
                             </form>
                         </div>
                     </div>
