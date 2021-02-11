d7184f1fe2456e0f2e7e56e050d5f0c98ba6b029
bamblebam <burhanizrangwala@gmail.com>
2021-02-12 00:29:17 +0530

    solved fontawesome issue

--- frontend/pages/auth/authentication.js
+++ frontend/pages/auth/authentication.js
@@ -1,14 +1,29 @@
 import React from 'react'
 import Head from 'next/head'
 import styles from '../../styles/css/authentication.module.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faUser } from "@fortawesome/free-solid-svg-icons"
 
 export default function authentication() {
     return (
         <>
             <Head>
+                <script src="https://use.fontawesome.com/ffe70d5ceb.js"></script>
             </Head>
             <div className={styles.body}>
-                <h1>hello</h1>
+                <div className={styles.main_container}>
+                    <div className={styles.form_container}>
+                        <div className={styles.signin_signup}>
+                            <form action="" method="POST" className={styles.signin_form}>
+                                <h2 className={styles.title}>
+                                    <div className={styles.input_field}>
+                                        <FontAwesomeIcon icon={faUser}></FontAwesomeIcon>
+                                    </div>
+                                </h2>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
