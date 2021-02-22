582479d84acea0b63d24bda7444ed3807cf5491c
bamblebam <burhanizrangwala@gmail.com>
2021-02-23 00:34:52 +0530

    donate page work start

---
+++ frontend/pages/product/donate.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Head from 'next/head'
+import styles from '../../styles/css/donate.module.css'
+
+export default function Donate() {
+    return (
+        <div className={styles.body}>
+            <div className={styles.main_container}>
+                <div className={styles.form_container}>
+                    <div className={styles.donate_form}>
+                        
+                    </div>
+                </div>
+                <div className={styles.panel_container}></div>
+            </div>
+        </div>
+    )
+}
