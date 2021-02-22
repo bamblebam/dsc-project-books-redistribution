d8c81eaaab2f9b35455bce35d6b98cc710fb9abd
bamblebam <burhanizrangwala@gmail.com>
2021-02-23 01:17:12 +0530

    basic form done

--- frontend/pages/product/donate.js
+++ frontend/pages/product/donate.js
@@ -7,11 +7,34 @@ export default function Donate() {
         <div className={styles.body}>
             <div className={styles.main_container}>
                 <div className={styles.form_container}>
-                    <div className={styles.donate_form}>
-                        
+                    <div className={styles.form}>
+                        <div className={styles.donate_form}>
+                            <div className={styles.content}>
+                                <div className={styles.title}>
+                                    <h1>Donate</h1>
+                                </div>
+                            </div>
+                            <div className={styles.input_field}>
+                                <input type="text" className={styles.input} placeholder="Product Name" name="product_name" />
+                            </div>
+                            <div className={styles.input_field}>
+                                <input type="text" className={styles.input} placeholder="Product Name" name="product_name" />
+                            </div>
+                            <div className={styles.input_field}>
+                                <input type="text" className={styles.input} placeholder="Product Name" name="product_name" />
+                            </div>
+                            <button className={styles.btn}>Donate</button>
+                        </div>
                     </div>
                 </div>
-                <div className={styles.panel_container}></div>
+                <div className={styles.panel_container}>
+                    <div className={styles.panel}>
+                        <div className={styles.content}>
+                            <h3>Donate here</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime tenetur, voluptatem quod ex ipsum, cumque doloremque neque quam quo accusamus asperiores non, officiis corporis. Maxime tenetur dignissimos sit recusandae animi?</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
