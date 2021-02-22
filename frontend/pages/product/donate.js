2f94c8ad0d8334651211f272663f8331b55925c8
bamblebam <burhanizrangwala@gmail.com>
2021-02-23 02:14:54 +0530

    file upload front bug

--- frontend/pages/product/donate.js
+++ frontend/pages/product/donate.js
@@ -8,7 +8,7 @@ export default function Donate() {
             <div className={styles.main_container}>
                 <div className={styles.form_container}>
                     <div className={styles.form}>
-                        <form method="POST" className={styles.donate_form}>
+                        <form method="POST" className={styles.donate_form} encType="multipart/form-data">
                             <div className={styles.content}>
                                 <div className={styles.title}>
                                     <h1>Donate</h1>
@@ -20,8 +20,8 @@ export default function Donate() {
                             <div className={styles.text_field}>
                                 <textarea cols="30" rows="10" className={styles.text} placeholder="Enter a short description about your product." name="product_description"></textarea>
                             </div>
-                            <div className={styles.input_field}>
-                                <input type="text" className={styles.input} placeholder="Product Name" name="product_name" />
+                            <div className={styles.file_field}>
+                                <input type="file" accept="image/*" className={styles.file} name="product_image" />
                             </div>
                             <button className={styles.btn}>Donate</button>
                             <p className={styles.subtext}>
