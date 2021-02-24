c63e07355df4bccd8f4c5af1ff01a5ec8c95df90
KrutikaBhatt <krutikabhatt222@gmail.com>
2021-02-24 23:59:41 +0530

    Replaced with Socket.io

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ const config = require('./config');
 const Userroute = require('./routes/UserRoutes');
 const Booksroute = require('./routes/BookRoutes');
 
+
 const app = express();
 
 app.use(express.json());
@@ -13,4 +14,29 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use('/api', Userroute.routes);
 app.use('/books',Booksroute.routes);
-app.listen(config.port, () => console.log("App is listening on url http://localhost:" + config.port))
+const server = app.listen(config.port, () => console.log("App is listening on url http://localhost:" + config.port))
+
+// Added Socket.io instansiation
+const io = require('socket.io')(server);
+
+//On connection
+io.on('connection', (socket) => {
+	console.log('New user connected')
+
+    socket.username = "World-changer"
+
+    socket.on('change_username', (data) => {
+        socket.username = data.username
+    })
+
+    //listen on new_message
+    socket.on('new_message', (data) => {
+        //broadcast the new message
+        io.sockets.emit('new_message', {message : data.message, username : socket.username});
+    })
+
+    //listen on typing
+    socket.on('typing', (data) => {
+    	socket.broadcast.emit('typing', {username : socket.username})
+    })
+})
