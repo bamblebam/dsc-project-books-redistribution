4a0c6330573eda780fa10f27663535edc7203395
therookie636 <therookie636@gmail.com>
2021-03-19 13:27:53 +0530

    added styles to resources page

--- frontend/pages/about/about.js
+++ frontend/pages/about/about.js
@@ -6,39 +6,39 @@ import aboutStyles from '../../styles/css/about.module.css'
 import Footer from '../../components/Footer'
 
 const about = () => {
-	// useEffect(() => {
-	// 	gsap.registerPlugin(ScrollTrigger)
-	// 	const t1 = gsap.timeline()
-	// 	let tween1 = gsap.from('#left', {
-	// 		scrollTrigger: {
-	// 			trigger: '#left',
-	// 			start: '50px 50%',
-	// 			end: 'bottom bottom',
-	// 			toggleActions: 'play none none none',
-	// 			markers: true,
-	// 		},
-	// 		opacity: 0,
-	// 		x: '-=100px',
-	// 		duration: 0.5,
-	// 	})
-	// 	let tween2 = gsap.from('#right', {
-	// 		scrollTrigger: {
-	// 			trigger: '#right',
-	// 			start: '50px 50%',
-	// 			end: 'bottom bottom',
-	// 			toggleActions: 'play none none none',
-	// 			markers: true,
-	// 		},
-	// 		opacity: 0,
-	// 		x: '-=100px',
-	// 		duration: 0.5,
-	// 		delay: 1.5,
-	// 	})
-	// }, [])
+	useEffect(() => {
+		gsap.registerPlugin(ScrollTrigger)
+		const t1 = gsap.timeline()
+		let tween1 = gsap.from('#left', {
+			scrollTrigger: {
+				trigger: '#left',
+				start: '50px 50%',
+				end: 'bottom bottom',
+				toggleActions: 'play none none none',
+				markers: true,
+			},
+			opacity: 0,
+			x: '-=100px',
+			duration: 0.5,
+		})
+		let tween2 = gsap.from('#right', {
+			scrollTrigger: {
+				trigger: '#right',
+				start: '50px 50%',
+				end: 'bottom bottom',
+				toggleActions: 'play none none none',
+				markers: true,
+			},
+			opacity: 0,
+			x: '-=100px',
+			duration: 0.5,
+			delay: 1.5,
+		})
+	}, [])
 	return (
 		<div>
 			<div className={aboutStyles.container}>
-				<div id='left' className={aboutStyles.details}>
+				<div className={aboutStyles.details}>
 					<h1 className={aboutStyles.title}>How?</h1>
 					<p>
 						People who have book they are willing to donate, add their books in the
@@ -46,7 +46,7 @@ const about = () => {
 					</p>
 					<p>People who want the books can take them from item list</p>
 				</div>
-				<div id='right' className={aboutStyles.image}>
+				<div className={aboutStyles.image}>
 					<Image
 						src='/images/bookshelf.jpg'
 						alt='test image'
