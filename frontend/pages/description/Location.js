9b47266b506111d4ec71bd783db0a3886410e86d
KrutikaBhatt <krutikabhatt222@gmail.com>
2021-03-22 18:03:28 +0530

    Location added

--- frontend/pages/description/Location.js
+++ frontend/pages/description/Location.js
@@ -1,11 +1,14 @@
 import React, { useRef ,useState} from 'react'
 import Head from 'next/head'
 import Map from './Map'
-const description = () => {
+const Location = () => {
     return(
         <div>
             <Map>
             </Map>
         </div>
     );
-}
+};
+
+
+export default Location;
