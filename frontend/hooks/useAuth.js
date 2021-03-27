92edba5d53fc82d9f2c2e55ab997fda3f15d0671
bamblebam <burhanizrangwala@gmail.com>
2021-03-28 00:37:13 +0530

    hook and basic signup

---
+++ frontend/hooks/useAuth.js
@@ -0,0 +1,26 @@
+import { createContext, useContext, useState } from "react"
+import { auth } from "../configurations/db.js"
+
+const authContext = createContext({ user })
+const { Provider } = authContext
+
+export function AuthProvider(props) {
+    const auth = useAuthProvider()
+    return <Provider value={auth}>{props.children}</Provider>
+}
+
+export const useAuth = () => {
+    return useContext(authContext)
+}
+
+const useAuthProvider = () => {
+    const [user, setUser] = useState(null)
+    const signup = ({ email, password }) => {
+        return auth.createUserWithEmailAndPassword(email, password).then(res => {
+            console.log(res)
+        }).catch(error => {
+            console.log(error)
+        })
+    }
+    return { user, signup }
+}
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -2,13 +2,16 @@ import "../styles/globals.css";
 import Layout from "../components/Layout";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
+import { AuthProvider } from "../hooks/useAuth";
 config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }) {
 	return (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
+		<AuthProvider>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</AuthProvider>
 	);
 }
 
